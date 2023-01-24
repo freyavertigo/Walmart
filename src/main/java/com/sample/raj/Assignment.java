48bd70612e75a83fcc4589a25123f9e3d422c8cd
iterraj0@gmail.com <rajya@LAPTOP-FLKOVKE0.lan>
2023-01-24 15:03:19 -0600

    Added

---
+++ src/main/java/com/sample/raj/Assignment.java
@@ -0,0 +1,52 @@
+package com.sample.raj;
+
+public class Assignment {
+
+	public static void main() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 5; i++) {
+			  System.out.println(i);
+			}
+		int i = 0;
+		while (i < 5) {
+		  System.out.println(i);
+		  i++;
+		}
+		int x = 20;
+		int y = 18;
+		if (x > y) {
+		  System.out.println("x is greater than y");
+		}
+		int time = 20;
+		if (time < 18) {
+		  System.out.println("Good day.");
+		} else {
+		  System.out.println("Good evening.");
+		}
+		int day = 4;
+		switch (day) {
+		  case 1:
+		    System.out.println("Monday");
+		    break;
+		  case 2:
+		    System.out.println("Tuesday");
+		    break;
+		  case 3:
+		    System.out.println("Wednesday");
+		    break;
+		  case 4:
+		    System.out.println("Thursday");
+		    break;
+		  case 5:
+		    System.out.println("Friday");
+		    break;
+		  case 6:
+		    System.out.println("Saturday");
+		    break;
+		  case 7:
+		    System.out.println("Sunday");
+		    break;
+		}
+	}
+
+}
